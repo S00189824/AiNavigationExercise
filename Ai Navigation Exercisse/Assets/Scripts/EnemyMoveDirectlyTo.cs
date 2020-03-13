bf68b212ff2274ae4c2075680860f01c48743b61
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-13 17:53:02 +0000

    gfdg

---
+++ Ai Navigation Exercisse/Assets/Scripts/EnemyMoveDirectlyTo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveDirectlyTo : NavMeshMover
+{
+    public string tagToTrack = "Player";
+    GameObject trackedPlayer;
+
+
+    public override void Start()
+    {
+        trackedPlayer = GameObject.FindGameObjectWithTag(tagToTrack);
+
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trackedPlayer != null)
+        {
+            MoveTo(trackedPlayer);
+        }
+    }
+}
---
+++ Ai Navigation Exercisse/Assets/Scripts/PlayerNavMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavMover : NavMeshMover
+{
+
+    public GameObject PlayerController;
+
+    public Color DebugLineColor { get; private set; }
+
+
+    // Use this for initialization
+    public override void Start()
+    {
+        PlayerController.GetComponent<PlayerMovementRayCast>().RayCastReady += PlayerNavMover_RayCastReady;
+        base.Start();
+	}
+
+    private void PlayerNavMover_RayCastReady(RaycastHit selectionData)
+    {
+        MoveTo(selectionData.point);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if(agent != null)
+        {
+            if(agent.pathStatus != NavMeshPathStatus.PathInvalid)
+            {
+                for (int i = 0; i < agent.path.corners.Length; i++)
+                {
+                    if(i + 1 < agent.path.corners.Length)
+                    {
+                        Gizmos.color = DebugLineColor;
+                        Gizmos.DrawLine(agent.path.corners[i], agent.path.corners[i + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
